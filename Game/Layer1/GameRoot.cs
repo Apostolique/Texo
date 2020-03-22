5271ac64062796b7b7e8807d803035e439a06161
Jean-David Moisan <vynecompiler@gmail.com>
2020-03-22 17:44:28 -0400

    Initial project

---
+++ Game/Layer1/GameRoot.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameProject {
+    public class GameRoot : Game {
+        public GameRoot() {
+            _graphics = new GraphicsDeviceManager(this);
+            IsMouseVisible = true;
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize() {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            _s = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _sampleImage = Content.Load<Texture2D>("SampleImage");
+        }
+
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            _s.Begin();
+            _s.Draw(_sampleImage, Vector2.Zero, Color.White);
+            _s.End();
+
+            base.Draw(gameTime);
+        }
+
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _s;
+        Texture2D _sampleImage;
+    }
+}
